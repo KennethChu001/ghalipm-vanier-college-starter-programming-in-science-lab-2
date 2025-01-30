336929cd3012c701e51e1cc12ee0f75c0fdf3064
KennethChu001 <kennethloksunchu@gmail.com>
2025-01-30 13:43:34 -0500

    Update Lab2.py
    First commit

--- Lab2.py
+++ Lab2.py
@@ -2,10 +2,23 @@
 # This function should take the initial height h0 and time t as inputs, and return the height at time t.
 def calculate_height(h0, t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    h0=float(input("Enter initial height: "))
+    time=float(input("Enter time: "))
+    g=9.8
+    h=h0-0.5*g*time**2
+    print("Height of the ball at time", time, "second = ", h)
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
-    pass  # Replace with your code
+    time=float(input("Enter time for car (in seconds): "))
+    speed=20
+    distance=time*speed
+    print("The car will travel", distance, "meters in", time, "second.")
+    time2=float(input("Enter time for car (in seconds): "))
+    distance=time2*speed
+    print("The car will travel", distance, "meters in", time2, "second.")
+    time3=float(input("Enter time for car (in seconds): "))
+    distance=time3*speed
+    print("The car will travel", distance, "meters in", time3, "second.")
